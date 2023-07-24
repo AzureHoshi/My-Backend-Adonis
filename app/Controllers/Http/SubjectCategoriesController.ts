0ceca2b5a8dc5c2ffa3774bf71b25d01f95043af
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-07-24 14:20:56 +0700

    SubjectCategories is done

--- app/Controllers/Http/SubjectCategoriesController.ts
+++ app/Controllers/Http/SubjectCategoriesController.ts
@@ -1,7 +1,77 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { schema, rules } from "@ioc:Adonis/Core/Validator";
+import SubjectCategory from "App/Models/SubjectCategory";
 
+const subjectCategorySchema = schema.create({
+  subject_category_name: schema.string([rules.maxLength(255)]),
+});
 export default class SubjectCategoriesController {
   public async index({ response }: HttpContextContract) {
-    return response.json({ test: "test" });
+    const subjectCategories = await SubjectCategory.all();
+    return response.status(200).json({ data: subjectCategories, status: 200 });
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    try {
+      const payload = await request.validate({ schema: subjectCategorySchema });
+      const subjectCategory: SubjectCategory = await SubjectCategory.create(
+        payload
+      );
+      return response.status(201).json({ data: subjectCategory, status: 201 });
+    } catch (error) {
+      return response.status(400).json({ error: error.message, status: 400 });
+    }
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const id = params.id;
+    try {
+      const payload = await request.validate({ schema: subjectCategorySchema });
+      const subjectCategory: any = await SubjectCategory.find(id);
+      if (!subjectCategory) {
+        return response
+          .status(404)
+          .json({ message: "SubjectCategory not found", status: 404 });
+      } else {
+        subjectCategory.merge(payload);
+        await subjectCategory.save();
+        return response.status(200).json({
+          data: subjectCategory,
+          status: 200,
+          message: `SubjectCategory updated byId ${id} success`,
+        });
+      }
+    } catch {
+      return response
+        .status(400)
+        .json({ error: "Incorrect or incomplete information", status: 400 });
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    const id = params.id;
+    try {
+      const subjectCategory: any = await SubjectCategory.find(id);
+      if (!subjectCategory) {
+        return response
+          .status(404)
+          .json({ message: "SubjectCategory not found", status: 404 });
+      } else if (subjectCategory.is_deleted == true) {
+        return response
+          .status(400)
+          .json({ message: "SubjectCategory already deleted", status: 400 });
+      } else {
+        subjectCategory.is_deleted = true;
+        await subjectCategory.save();
+        return response.status(200).json({
+          message: `SubjectCategory deleted byId ${id} success`,
+          status: 200,
+        });
+      }
+    } catch {
+      return response
+        .status(400)
+        .json({ error: "Incorrect or incomplete information", status: 400 });
+    }
   }
 }
