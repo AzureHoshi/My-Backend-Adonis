670c1ffbfd383679d09b91edef568a32f6667924
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-08-27 18:24:21 +0700

    Fix store data with [{}]

--- app/Controllers/Http/CollegianFeedbacksController.ts
+++ app/Controllers/Http/CollegianFeedbacksController.ts
@@ -1,93 +1,48 @@
-import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { schema } from "@ioc:Adonis/Core/Validator";
 import CollegianFeedback from "App/Models/CollegianFeedback";
 
-const collegianFeedbackSchema = schema.create({
-  collegian_id: schema.number(),
-  feedback_id: schema.number(),
-  feedback_answer_id: schema.number(),
-});
+const collegianFeedbackSchema = schema.array().members(
+  schema.object().members({
+    collegian_id: schema.number(),
+    feedback_id: schema.number(),
+    feedback_answer_id: schema.number(),
+  })
+);
 
 export default class CollegianFeedbacksController {
-  public async index({ response }: HttpContextContract) {
-    const collegianFeedbacks = await CollegianFeedback.query()
-      .preload("collegian")
-      .preload("feedback")
-      .preload("feedbackAnswer")
-      .where("collegian_feedbacks.is_deleted", false)
-      .orderBy("collegian_feedbacks.updated_at", "desc");
-    return response.status(200).json({ data: collegianFeedbacks, status: 200 });
-  }
-
   public async store({ request, response }: HttpContextContract) {
     try {
-      const payload = await request.validate({
-        schema: collegianFeedbackSchema,
+      const rawData = request.raw();
+      const { data } = JSON.parse(rawData);
+
+      const collegianFeedbacks = await Promise.all(
+        data.map(async (collegianFeedback) => {
+          const newCollegianFeedback = await CollegianFeedback.create(
+            collegianFeedback
+          );
+          return newCollegianFeedback;
+        })
+      );
+
+      return response.status(201).json({
+        data: collegianFeedbacks,
+        status: 201,
+        message: "CollegianFeedback created success",
       });
-      const collegianFeedback: CollegianFeedback =
-        await CollegianFeedback.create(payload);
-      return response
-        .status(201)
-        .json({ data: collegianFeedback, status: 201 });
     } catch (error) {
-      return response
-        .status(400)
-        .json({ message: "Incorrect or incomplete information", status: 400 });
+      let errorMessage = "Validation failed";
+      if (error?.messages?.errors?.length > 0) {
+        errorMessage = error.messages.errors.join(", ");
+      } else if (error?.message) {
+        errorMessage = error.message;
+      }
+      return response.status(400).json({
+        message: errorMessage,
+        status: 400,
+      });
     }
   }
 
-  public async update({ params, request, response }: HttpContextContract) {
-    try {
-      const id = params.id;
-      const payload = await request.validate({
-        schema: collegianFeedbackSchema,
-      });
-      const collegianFeedback: any = await CollegianFeedback.find(id);
-      if (!collegianFeedback) {
-        return response
-          .status(404)
-          .json({ message: "CollegianFeedback not found", status: 404 });
-      } else {
-        collegianFeedback.merge(payload);
-        await collegianFeedback.save();
-        return response.status(200).json({
-          data: collegianFeedback,
-          status: 200,
-          message: `CollegianFeedback updated byId ${id} success`,
-        });
-      }
-    } catch (error) {
-      return response
-        .status(400)
-        .json({ message: "incorrect or incomplete information", status: 400 });
-    }
-  }
-
-  public async destroy({ params, response }: HttpContextContract) {
-    try {
-      const id = params.id;
-      const collegianFeedback: any = await CollegianFeedback.find(id);
-      if (!collegianFeedback) {
-        return response
-          .status(404)
-          .json({ message: "CollegianFeedback not found", status: 404 });
-      } else if (collegianFeedback.is_deleted) {
-        return response
-          .status(404)
-          .json({ message: "CollegianFeedback already deleted", status: 404 });
-      } else {
-        collegianFeedback.is_deleted = true;
-        await collegianFeedback.save();
-        return response.status(200).json({
-          data: collegianFeedback,
-          status: 200,
-          message: `CollegianFeedback deleted byId ${id} success`,
-        });
-      }
-    } catch (error) {
-      return response
-        .status(400)
-        .json({ message: "incorrect or incomplete information", status: 400 });
-    }
-  }
+  // Other methods like index, update, destroy, etc.
 }
