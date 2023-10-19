69e39da393278f9dcddc62fdf534f29082914272
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-10-19 20:24:40 +0700

    add data in seeder Faculty

--- database/seeders/Faculty.ts
+++ database/seeders/Faculty.ts
@@ -3,16 +3,30 @@ import Faculty from "App/Models/Faculty";
 
 export default class extends BaseSeeder {
   public async run() {
-    // Write your database queries inside the run method
-    await Faculty.createMany([
+    // เพิ่มข้อมูลคณะเข้าฐานข้อมูล
+    const facultyData = [
       {
-        email: "virk@adonisjs.com",
-        password: "secret",
+        faculty_name_th: "คณะบริหารธุรกิจและศิลปศาสตร์",
+        faculty_name_en: "Faculty of Business Administration and Liberal Arts",
       },
       {
-        email: "romain@adonisjs.com",
-        password: "supersecret",
+        faculty_name_th: "คณะวิทยาศาสตร์และเทคโนโลยีการเกษตร",
+        faculty_name_en: "Faculty of Sciences and Agricultural Technology",
       },
-    ]);
+      {
+        faculty_name_th: "คณะวิศวกรรมศาสตร์",
+        faculty_name_en: "Faculty of Engineering",
+      },
+      {
+        faculty_name_th: "คณะศิลปกรรมกรรมและสถาปัตยกรรมศาสตร์",
+        faculty_name_en: "Faculty of Fine Arts and Architecture",
+      },
+      {
+        faculty_name_th: "วิทยาลัยเทคโนโลยีและสหวิทยาการ",
+        faculty_name_en: "College of Integrated Science and Technology",
+      },
+    ];
+
+    await Faculty.createMany(facultyData);
   }
 }
