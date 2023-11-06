821d1cea3999e33a1a1cc8346be89e707af1fcce
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-11-06 15:57:32 +0700

    create route curriculum-structures get by id

--- app/Controllers/Http/CurriculumStructuresController.ts
+++ app/Controllers/Http/CurriculumStructuresController.ts
@@ -34,6 +34,45 @@ export default class CurriculumStructuresController {
     }
   }
 
+  public async show({ params, response }: HttpContextContract) {
+    try {
+      const id = params.id;
+      const curriculumStructure = await CurriculumStructure.query()
+        .preload("curriculum")
+        .preload("subject_group")
+        .where("curriculum_id", id)
+        .firstOrFail();
+
+      if (!curriculumStructure) {
+        return response
+          .status(404)
+          .json({ message: "CurriculumStructure not found", status: 404 });
+      } else if (curriculumStructure.is_deleted === true || 1) {
+        return response.status(404).json({
+          message: "CurriculumStructure already deleted",
+          status: 404,
+        });
+      } else {
+        const subjectType = await SubjectType.query()
+          .where(
+            "subject_type_id",
+            curriculumStructure.subject_group.subject_type_id
+          )
+          .firstOrFail();
+
+        return response.status(200).json({
+          data: {
+            ...curriculumStructure.serialize(),
+            subject_type: subjectType,
+          },
+          status: 200,
+        });
+      }
+    } catch (error) {
+      return response.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
   public async store({ request, response }: HttpContextContract) {
     const validationSchema = schema.create({
       curriculum_id: schema.number(),
