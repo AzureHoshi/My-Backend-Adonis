d8bd9b5c087bf597847f1e2d8f6d70772c023c8a
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-11-16 16:13:08 +0700

    Fixed add children: [] then not find children at showBySubjectId ContinueSubjectsController

--- app/Controllers/Http/ContinueSubjectsController.ts
+++ app/Controllers/Http/ContinueSubjectsController.ts
@@ -136,6 +136,7 @@ export default class ContinueSubjectsController {
           } else {
             return {
               ...item.$attributes,
+              children: [],
             };
           }
         })
