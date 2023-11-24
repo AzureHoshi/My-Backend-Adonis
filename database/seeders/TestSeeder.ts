3eb2a336431a032bccd25902a417afce9d5ab807
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-11-24 16:37:14 +0700

    Fixed seeder

--- database/seeders/TestSeeder.ts
+++ database/seeders/TestSeeder.ts
@@ -4,6 +4,7 @@ import Competency from "App/Models/Competency";
 import CompetencySub from "App/Models/CompetencySub";
 import ContinueSubject from "App/Models/ContinueSubject";
 import Curriculum from "App/Models/Curriculum";
+import CurriculumStructuresV2 from "App/Models/CurriculumStructuresV2";
 import Faculty from "App/Models/Faculty";
 import Feedback from "App/Models/Feedback";
 import InterestAnswer from "App/Models/InterestAnswer";
@@ -2512,6 +2513,104 @@ export default class extends BaseSeeder {
         subject_id: 88,
       },
     ];
+    const curriculumStructureV2Data = [
+      {
+        // 1
+        curriculum_id: 1,
+        subject_category_id: 1,
+        subject_type_id: 1,
+        subject_group_id: 1,
+        credit_total: 12,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 1,
+        subject_type_id: 1,
+        subject_group_id: 2,
+        credit_total: 3,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 1,
+        subject_type_id: 1,
+        subject_group_id: 3,
+        credit_total: 9,
+      },
+      {
+        // 2
+        curriculum_id: 1,
+        subject_category_id: 1,
+        subject_type_id: 2,
+        subject_group_id: 4,
+        credit_total: 3,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 1,
+        subject_type_id: 2,
+        subject_group_id: 5,
+        credit_total: 4,
+      },
+      {
+        // 3
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 3,
+        subject_group_id: 6,
+        credit_total: 10,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 3,
+        subject_group_id: 7,
+        credit_total: 22,
+      },
+      {
+        // 4
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 4,
+        subject_group_id: 8,
+        credit_total: 16,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 4,
+        subject_group_id: 9,
+        credit_total: 12,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 4,
+        subject_group_id: 10,
+        credit_total: 9,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 4,
+        subject_group_id: 11,
+        credit_total: 3,
+      },
+      {
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 4,
+        subject_group_id: 12,
+        credit_total: 10,
+      },
+      {
+        // 5
+        curriculum_id: 1,
+        subject_category_id: 2,
+        subject_type_id: 5,
+        subject_group_id: null,
+        credit_total: 12,
+      },
+    ];
 
     await Faculty.createMany(facultyData);
     await CollegianGroup.createMany(collegianGroupData);
@@ -2532,5 +2631,6 @@ export default class extends BaseSeeder {
     await JobPosition.createMany(jobPositionData);
     await InterestAnswerJob.createMany(interestAnswersJobData);
     await SubjectStructure.createMany(subjectStructuresData);
+    await CurriculumStructuresV2.createMany(curriculumStructureV2Data);
   }
 }
