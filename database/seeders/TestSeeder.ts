48fb023141fd0dd6c5edab450839286d0acd5dc2
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-11-28 16:50:49 +0700

    Fixed to default

--- database/seeders/TestSeeder.ts
+++ database/seeders/TestSeeder.ts
@@ -22,12 +22,32 @@ import SubjectGroup from "App/Models/SubjectGroup";
 import SubjectStructure from "App/Models/SubjectStructure";
 import SubjectType from "App/Models/SubjectType";
 
-// import json
-import facultyData from "./json/faculties";
-
 export default class extends BaseSeeder {
   public async run() {
     // Write your database queries inside the run method
+
+    const facultyData = [
+      {
+        faculty_name_th: "คณะบริหารธุรกิจและศิลปศาสตร์",
+        faculty_name_en: "Faculty of Business Administration and Liberal Arts",
+      },
+      {
+        faculty_name_th: "คณะวิทยาศาสตร์และเทคโนโลยีการเกษตร",
+        faculty_name_en: "Faculty of Sciences and Agricultural Technology",
+      },
+      {
+        faculty_name_th: "คณะวิศวกรรมศาสตร์",
+        faculty_name_en: "Faculty of Engineering",
+      },
+      {
+        faculty_name_th: "คณะศิลปกรรมกรรมและสถาปัตยกรรมศาสตร์",
+        faculty_name_en: "Faculty of Fine Arts and Architecture",
+      },
+      {
+        faculty_name_th: "วิทยาลัยเทคโนโลยีและสหวิทยาการ",
+        faculty_name_en: "College of Integrated Science and Technology",
+      },
+    ];
 
     const collegianGroupData = [
       {
