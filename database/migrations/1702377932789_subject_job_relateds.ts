2b81d4010a8afbbcbf39e81efbe8b5fc84a4be52
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-12-25 17:24:05 +0700

    Fixed migration subject_job_relateds

--- database/migrations/1702377932789_subject_job_relateds.ts
+++ database/migrations/1702377932789_subject_job_relateds.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("subjects_jobs_related_id");
+      table.increments("subject_job_related_id");
       table
         .integer("subject_id")
         .unsigned()
