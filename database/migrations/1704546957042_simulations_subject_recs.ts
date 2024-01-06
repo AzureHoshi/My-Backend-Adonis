a12dddbd8070727ff388392c75e296e177c78f22
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2024-01-06 20:28:48 +0700

    feat: - create migration simulations_subject_recs and simulations_job_recs fix: collegian_code type in feedback_records migration

--- database/migrations/1704546957042_simulations_subject_recs.ts
+++ database/migrations/1704546957042_simulations_subject_recs.ts
@@ -1,14 +1,23 @@
 import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = "feedback_records";
+  protected tableName = "simulations_subject_recs";
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("feedback_record_id");
-      table.string("collegian_code", 13).notNullable().comment("รหัสนักศึกษา");
-      table.text("feedback_record_answer").notNullable();
+      table.increments("sim_sub_rec_id");
+      table.string("collegian_code", 13).comment("รหัสนักศึกษา");
+      table
+        .integer("subject_id")
+        .unsigned()
+        .notNullable()
+        .references("subject_id")
+        .inTable("subjects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")
+        .comment("รหัสวิชา");
 
+      table.boolean("is_deleted").defaultTo(0).comment("สถานะการลบ");
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
--- database/migrations/1704547421701_simulations_job_recs.ts
+++ database/migrations/1704547421701_simulations_job_recs.ts
@@ -1,14 +1,23 @@
 import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = "feedback_records";
+  protected tableName = "simulations_job_recs";
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("feedback_record_id");
-      table.string("collegian_code", 13).notNullable().comment("รหัสนักศึกษา");
-      table.text("feedback_record_answer").notNullable();
+      table.increments("sim_job_rec_id");
+      table.string("collegian_code", 13).comment("รหัสนักศึกษา");
+      table
+        .integer("job_position_id")
+        .unsigned()
+        .notNullable()
+        .references("job_position_id")
+        .inTable("job_positions")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")
+        .comment("รหัสตำแหน่งงาน");
 
+      table.boolean("is_deleted").defaultTo(0).comment("สถานะการลบ");
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
