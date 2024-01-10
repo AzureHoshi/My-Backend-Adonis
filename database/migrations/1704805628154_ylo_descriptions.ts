031585be80029ad9196eb5e5432e58d636b5d74e
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2024-01-10 20:36:10 +0700

    fix: change migration ylo_descriptions col ylo_description_id to ylo_des_id

--- database/migrations/1704805628154_ylo_descriptions.ts
+++ database/migrations/1704805628154_ylo_descriptions.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("ylo_description_id").primary();
+      table.increments("ylo_des_id").primary();
       table
         .integer("ylo_id")
         .notNullable()
